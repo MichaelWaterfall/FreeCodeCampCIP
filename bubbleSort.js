0e66ee64eceb0c3bc89b00ef5ad171b3c23ab009
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-14 14:21:04 +0000

    bubbleSort added

---
+++ bubbleSort.js
@@ -0,0 +1,8 @@
+function bubbleSort(array) {
+    // Only change code below this line
+    
+    return newArray;
+    // Only change code above this line
+    //[5, 8, 12, 1]
+    //[1, 5, 8, 12]
+  }
