261d4f608caa4379f0c527b1b3e915255bc37be1
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-14 14:22:13 +0000

    bubbleSort.js test

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,13 @@
 function bubbleSort(array) {
     // Only change code below this line
-    
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+      if(array[i] > array[i + 1]) {
+        newArray.push(array[i]);
+      } else {
+        newArray.push(array[i + 1]);
+      }
+    }
     return newArray;
     // Only change code above this line
     //[5, 8, 12, 1]
