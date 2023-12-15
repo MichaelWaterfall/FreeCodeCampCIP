d2115f1a1172e4b833fb551e5e00f89fea795b5b
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-15 20:04:40 +0000

    Test smallestNumber

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,16 @@
-const array = [5, 8, 12, 1];
+const array = [21, 8, 1, 5, 11];
 let newArray = [];
 
 function bubbleSort(array) {
     // Only change code below this line
+    let smallestNumber;
     for(let i = 0; i < array.length; i++) {
       if(array[i] > array[i + 1]) {
-        newArray.push(array[i]);
-      } else {
-        newArray.push(array[i + 1]);
+        smallestNumber = array[i + 1];
       }
+      
     }
+    newArray.push(smallestNumber);
     return newArray;
     // Only change code above this line
     //[5, 8, 12, 1]
@@ -18,4 +19,5 @@ function bubbleSort(array) {
 
   bubbleSort(array);
 
+  console.log(array);
   console.log(newArray);
