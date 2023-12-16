f9247752d364563fe96d204772c5027aa4e9ec3f
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-16 21:07:16 +0000

    bubbleSort.js solved

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,17 +1,22 @@
-const array = [21, 8, 1, 5, 11];
+let array = [21, 8, 1, 5, 11];
 let newArray = [];
 
 function bubbleSort(array) {
     // Only change code below this line
-    let smallestNumber;
-    for(let i = 0; i < array.length; i++) {
-      if(array[i] > array[i + 1]) {
-        smallestNumber = array[i + 1];
-      }
-      
-    }
-    newArray.push(smallestNumber);
-    return newArray;
+    let swapped = true;
+    while(swapped) {
+        swapped = false;
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] > array[i + 1]) {
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } 
+    
+    return array;
     // Only change code above this line
     //[5, 8, 12, 1]
     //[1, 5, 8, 12]
