71ab653321d93993d70d25413de120e0481020a1
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-17 13:13:50 +0000

    while loop

--- selectionSort.js
+++ selectionSort.js
@@ -1,12 +1,27 @@
 let array = [5, 1, 11, 9, 2];
+let newArray = [];
 
 function selectionSort(array) {
     // Only change code below this line
-
-    return array;
+    let smallestNumber = array[0];
+    let swapped = true;
+    while(swapped) {
+        swapped = false;
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] < smallestNumber) {
+                smallestNumber = array[i];
+                newArray.push(smallestNumber);
+                array.splice(array[i], 1);
+                swapped = true;
+            }
+        }
+    } 
+    console.log(smallestNumber);
+    return newArray;
     // Only change code above this line
   }
 
 selectionSort(array);
 
 console.log(array);
+console.log(newArray);
