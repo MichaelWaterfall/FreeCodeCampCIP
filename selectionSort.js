686af31da685c372f49328ba16c78282eabb8466
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-17 12:41:33 +0000

    selectionSort.js file added

---
+++ selectionSort.js
@@ -0,0 +1,12 @@
+let array = [5, 1, 11, 9, 2];
+
+function selectionSort(array) {
+    // Only change code below this line
+
+    return array;
+    // Only change code above this line
+  }
+
+selectionSort(array);
+
+console.log(array);
